5912950376240b705bb809cec1180c4c4989a0d3
cah-jaishri-thakur <jaishri.thakur@caridnalhealth.com>
2022-05-25 15:30:27 -0400

    Created a bar chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyChartComponent } from './Components/my-chart/my-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyChartComponent
   ],
   imports: [
     BrowserModule,
